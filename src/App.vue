79be00636b765eaed18a735a3fc1513db7ea3794
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2023-01-16 19:14:40 +0600

    add test

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Modal, Toolbar, Grid } from '@/components';
+import { useGame, useModal } from '@/hooks';
+
+const { onCell, onClear, onToggle, grid, generation, running, numCols } =
+  useGame();
+const { onClose, onInfo, showing } = useModal();
+</script>
+
+<template>
+  <Toolbar
+    :running="running"
+    :generation="generation"
+    @toggle="onToggle"
+    @clear="onClear"
+    @info="onInfo"
+  />
+  <Grid :num-cols="numCols" :grid="grid" @cell="onCell" />
+  <Modal :showing="showing" @close="onClose" />
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Inter', sans-serif;
+  text-align: center;
+}
+</style>
